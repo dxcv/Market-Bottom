0bd06edd55c46d6ef87785aaa3abb896d427ef95
ZhehaoLi9705 <511267512@qq.com>
2018-07-31 20:46:59 +0800

    Today Last Change

--- Bottom.py
+++ Bottom.py
@@ -28,7 +28,7 @@ price_data = pd.read_pickle('/Users/trevor/Downloads/price.pkl')
 indicator_data = pd.read_pickle('/Users/trevor/Downloads/indicator.pkl')
 market_index = pd.read_excel('/Users/trevor/Downloads/市场底部特征研究.xlsx',sheet_name='沪深300')
 M2_data = pd.read_excel('/Users/trevor/Downloads/M2指标.xlsx')
-IPO_date = pd.read_excel('/Users/trevor/Downloads/A股上市时间.xlsx')
+IPO_data = pd.read_excel('/Users/trevor/Downloads/A股上市时间.xlsx')
 amount_data = pd.read_csv('/Users/trevor/Downloads/eodprices.csv')
 
 ###############################################################################
@@ -41,8 +41,9 @@ amount_data['TRADE_DT'] = amount_data['TRADE_DT'].apply(lambda x:str(x))
 finance_data = pd.merge(finance_data,amount_data,how='left',on=['S_INFO_WINDCODE','TRADE_DT'])
 
 #提取日收盘价
-stock_close_data = pd.pivot_table(finance_data,values='S_DQ_ADJCLOSE',index='S_INFO_WINDCODE',columns='TRADE_DT')                   
+stock_close_data = pd.pivot_table(finance_data,values='S_DQ_CLOSE',index='S_INFO_WINDCODE',columns='TRADE_DT')                   
 stock_close_data = stock_close_data.sort_index(axis=1)
+stock_close_data.columns = pd.DatetimeIndex(stock_close_data.columns)
 stock_close_number = stock_close_data.count(axis=0)
 
 
@@ -108,6 +109,7 @@ M2_MV_ratio = temp_M2.values/temp_median.values
 #提取PE
 stock_PE_data = pd.pivot_table(finance_data,values='S_VAL_PE_TTM',index='S_INFO_WINDCODE',columns='TRADE_DT')
 stock_PE_data = stock_PE_data.sort_index(axis=1)
+stock_PE_data.columns = pd.DatetimeIndex(stock_PE_data.columns)
 stock_PE_median = stock_PE_data.median(axis=0)
 stock_PE_median.index = pd.DatetimeIndex(stock_PE_median.index)
 
@@ -136,8 +138,6 @@ total_amount.index = pd.DatetimeIndex(total_amount.index)
 '''
 6.个股成交额&成交量（个股流动性）
 '''
-
-
 #筛选低成交量&成交额个股
 low_volume = 1000 #(手)
 low_volume_stock = stock_volume_data[stock_volume_data<low_volume]
@@ -163,20 +163,63 @@ low_amount_percent.index = pd.DatetimeIndex(low_amount_percent.index)
 
 ###############################################################################
 '''
-8.次新股(上市四个月内)破发率（市场人气）
+8.次新股(上市四个月内)PE（市场人气）
 '''
 #获取所有股票发行价格和发行时间
-stock_close_data.columns = pd.DatetimeIndex(stock_close_data.columns)
-IPO_date['IPO_DATE'] = pd.DatetimeIndex(IPO_date['IPO_DATE'])
+IPO_data['IPO_DATE'] = pd.DatetimeIndex(IPO_data['IPO_DATE'])
 
-#筛选出时间横截面股票
-subprime_stock={}
+#筛选出时间横截面的次新股
+subnew_stock=pd.DataFrame()
 for day in stock_close_data.columns:
-    period = day - IPO_date['IPO_DATE']
-    subprime_stock[day] = IPO_date[period<120*Day()]
+    period = day - IPO_data['IPO_DATE']
+    subnew_stock[day] = IPO_data[period<365*Day()]['S_INFO_WINDCODE']
+    subnew_stock[day] = subnew_stock[day][period>1*Day()]
+
+subnew_number = subnew_stock.count(axis=0)
+
+
+subnew_PE = pd.DataFrame()
+for day in stock_close_data.columns:
+    temp_PE = stock_PE_data[day].reset_index()
+    temp_subnew = subnew_stock[day].dropna().reset_index()
+    temp_PE = temp_PE.reset_index()
+    pe = pd.merge(temp_subnew,temp_PE,how='left',on='index')
+    subnew_PE[day] = pe.iloc[:,-1]
     
+subnew_PE_median = subnew_PE.median(axis=0)
+
+diffences_PE_median = subnew_PE_median-stock_PE_median
+
+###############################################################################
+'''
+9. 股票破发率（市场人气）
+'''
+#时间横截面破发股比例
+def Lower_Stock(IPO_data,subnew_stock,stock_close_data,day):
+    
+    temp_subnew = subnew_stock[day].dropna() 
+    temp_subnew = temp_subnew.reset_index()
+    temp_subnew.columns = ['index','S_INFO_WINDCODE']
+    temp_subnew = temp_subnew.drop('index',axis=1)
+    
+    temp_stock = stock_close_data[day]
+    temp_stock = temp_stock.reset_index()
+    temp_stock.columns = ['S_INFO_WINDCODE','PRICE']
+    
+    temporary = pd.merge(temp_subnew,temp_stock,how='left',on='S_INFO_WINDCODE')
+    temp_IPO = IPO_data
+    temp_data = pd.merge(temporary,temp_IPO,how='left',on='S_INFO_WINDCODE')
+
+    lower_stock = temp_data[temp_data['IPO_PICE']>temp_data['PRICE']]['S_INFO_WINDCODE']
+    return len(lower_stock)
 
 
+lower_ipo_stock = []
+for day in stock_close_data.columns:
+    lower_ipo_stock.append(Lower_Stock(IPO_data,subnew_stock,stock_close_data,day))
+
+
+lower_ipo_percent = np.array(lower_ipo_stock)/subnew_number.values*100
 
 
 ###############################################################################
@@ -184,54 +227,30 @@ for day in stock_close_data.columns:
 c = market_index.iloc[0,-1]
 d = market_index.iloc[2,-1]
 
+assert stock_close_data.columns.shape==lower_ipo_percent.shape
 fig = plt.figure()
 ax1 = fig.add_subplot(111)
 ax1.grid(False)
-ax1.bar(low_amount_percent.index,low_amount_percent,width=3.5,linewidth=0.8,color='yellowgreen',label='低成交额股票占比',zorder=1)
-ax1.bar(low_volume_percent.index,low_volume_percent/1.5,width=3.5,linewidth=0.8,color='orange',label='低成交量股票占比',zorder=2)
-#.plot(M2_data.index, M2_data*a/b, color='purple',linewidth=0.8,label='低成交额基准线',zorder=3)
-ax1.set_ylabel('低成交量&成交额股票占比')#低成交额占比(小于100W)
+ax1.bar(stock_close_data.columns,lower_ipo_percent,width=3.5,linewidth=1.8,color='yellowgreen',label='次新股破发比例',zorder=1)
+#ax1.bar(low_volume_percent.index,low_volume_percent/1.5,width=3.5,linewidth=0.8,color='orange',label='低成交量股票占比',zorder=2)
+#ax1.plot(subnew_PE_median.index,subnew_PE_median, color='purple',linewidth=0.8,label='次新股PE中位数',zorder=3)
+ax1.set_ylabel('破发百分比')#低成交额占比(小于100W)
 #ax1.set_ylim(0,130)
 ax1.legend(loc='upper right')
 
 ax2 = ax1.twinx()
 ax2.grid(True)
-#ax2.plot(nationbonds_interest.index,1/nationbonds_interest,color='orange',linewidth=0.8,label='十年国债收益率倒数',zorder=4)
+#ax2.plot(subnew_PE_median.index,subnew_PE_median, color='purple',linewidth=0.8,label='次新股PE中位数',zorder=3)
+#ax2.plot(stock_PE_median.index,stock_PE_median,color='orange',linewidth=0.8,label='市场PE中位数',zorder=4)
 ax2.plot(market_index.columns,market_index.iloc[0,:],color='red',linewidth=0.8,label='上证综指',zorder=5)
 ax2.plot(market_index.columns,market_index.iloc[2,:]*c/d,color='blue',linewidth=0.8,label='深证成指',zorder=6)
-ax2.set_ylabel('指数')
-ax2.set_ylim(0,7000)
+ax2.set_ylabel('PE')
+#ax2.set_ylim(0,7000)
 ax2.legend(loc='upper left')
 ax2.set_xlabel('时间')
-plt.savefig('低成交量成交额占比.jpg',dpi=700)
+plt.savefig('次新股破发比例.jpg',dpi=700)
 
 ####################################################################
 
 
 
-
-
-
-#a = market_index.iloc[0,-1]
-#b = market_index.iloc[2,-1]
-'''
-fig = plt.figure()
-ax1 = fig.add_subplot(111)
-ax1.grid(False)
-#ax1.bar(market_volume.index, market_volume.values, width=3,linewidth=0.8,color='yellowgreen',label='全市场成交量',zorder=1)
-ax1.plot(stock_PE_median.index, stock_PE_median, color='purple',linewidth=0.8,label='PE中位数',zorder=1)
-ax1.set_ylabel('PE中位数')
-ax1.set_ylim(0,130)
-ax1.legend(loc='upper right')
-
-ax2 = ax1.twinx()
-ax2.grid(True)
-ax2.plot(nationbonds_interest.index,1/nationbonds_interest,color='orange',linewidth=0.8,label='十年国债收益率倒数',zorder=2)
-#ax2.plot(market_index.columns,market_index.iloc[0,:],color='purple',linewidth=0.8,label='上证综指',zorder=3)
-#ax2.plot(market_index.columns,market_index.iloc[2,:]*a/b,color='purple',linewidth=0.8,label='深证成指',zorder=4)
-ax2.set_ylabel('十年国债收益率倒数')
-ax2.set_ylim(0,1)
-ax2.legend(loc='upper left')
-ax2.set_xlabel('时间')
-plt.savefig('十年国债收益率倒数.jpg',dpi=700)
-'''
